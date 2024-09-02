90eab7654d0e717187e582e7b05b8e60ef819edd
Muhdan Fyan Syah Sofian <muhdanfyan@gmail.com>
2024-09-02 09:00:23 +0800

    update

---
+++ app/create/page.tsx
@@ -0,0 +1,12 @@
+const CreatePage = () => {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-slate-100">
+      <div className="bg-white rounded-sm shadow p-8">
+        <h1 className="text-2xl font-bold mb-5">Upload Image</h1>
+        
+      </div>
+    </div>
+  )
+}
+
+export default CreatePage
